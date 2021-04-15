458044fccb924b13988657636c5d43554405a3a4
Xie Yuheng <xyheme@gmail.com>
2021-04-15 20:48:27 +0800

    up

--- src/models/gitlab-library/gitlab-library.ts
+++ src/models/gitlab-library/gitlab-library.ts
@@ -1,30 +1,43 @@
-import { Library, LibraryConfig, Module } from "@cicada-lang/cicada"
+import { Library, LibraryConfig, Module, Syntax } from "@cicada-lang/cicada"
 import { Gitlab } from "@gitbeaker/browser"
 import { Base64 } from "js-base64"
 
 export class GitLabLibrary implements Library {
-  config: LibraryConfig
   requester: InstanceType<typeof Gitlab>
+  config: LibraryConfig
+  cached_mods: Map<string, Module>
+  host: string
+  project_id: string | number
+  project_dir: string
 
   constructor(opts: {
     requester: InstanceType<typeof Gitlab>
     config: LibraryConfig
+    cached_mods?: Map<string, Module>
+    host: string
+    project_id: string | number
+    project_dir: string
   }) {
     this.requester = opts.requester
     this.config = opts.config
+    this.cached_mods = opts.cached_mods || new Map()
+    this.host = opts.host
+    this.project_id = opts.project_id
+    this.project_dir = opts.project_dir
   }
 
   static async create(opts: {
     host: string
     token: string
-    path: string
-    root: string
+    project_id: string | number
+    project_dir: string
   }): Promise<GitLabLibrary> {
-    const { path, root, host, token } = opts
+    const { project_id, project_dir, host, token } = opts
+
     const requester = new Gitlab({ host, token })
     const file = await requester.RepositoryFiles.show(
-      path,
-      `${root}/library.json`,
+      project_id,
+      `${project_id}/library.json`,
       "master"
     )
     const text = Base64.decode(file.content)
@@ -33,11 +46,33 @@ export class GitLabLibrary implements Library {
     return new GitLabLibrary({
       requester,
       config,
+      host,
+      project_id,
+      project_dir,
     })
   }
 
-  load(path: string): Promise<Module> {
-    throw new Error("TODO")
+  async load(path: string): Promise<Module> {
+    const cached = this.cached_mods.get(path)
+    if (cached) {
+      return cached
+    }
+
+    const file = await this.requester.RepositoryFiles.show(
+      this.project_id,
+      `${this.project_dir}/${path}`,
+      "master"
+    )
+    const text = Base64.decode(file.content)
+     const stmts = Syntax.parse_stmts(text)
+
+    const mod = new Module({ library: this })
+    for (const stmt of stmts) {
+      await stmt.execute(mod)
+    }
+
+    this.cached_mods.set(path, mod)
+    return mod
   }
 
   paths(): Promise<Array<string>> {
