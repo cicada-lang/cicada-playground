207d9c1e4eccd056418ec0c44fef2bacc146a430
Xie Yuheng <xyheme@gmail.com>
2021-04-17 17:26:17 +0800

    up

--- src/models/github-library/github-library.ts
+++ src/models/github-library/github-library.ts
@@ -2,4 +2,30 @@ import { GitLibrary } from "@/models/git-library"
 import { LibraryConfig, Module, Syntax } from "@cicada-lang/cicada"
 import { Octokit } from "@octokit/rest"
 
-export class GitHubLibrary {}
+export class GitHubLibrary implements GitLibrary {
+  config: LibraryConfig
+
+  constructor(opts: {
+    config: LibraryConfig
+    cached_mods?: Map<string, Module>
+  }) {
+    this.config = opts.config
+    this.cached_mods = opts.cached_mods || new Map()
+  }
+
+  async load(path: string, opts?: { force?: boolean }): Promise<Module> {
+    throw new Error("TODO")
+  }
+
+  async fetch_files(): Promise<Map<string, string>> {
+    throw new Error("TODO")
+  }
+
+  async load_mods(): Promise<Map<string, Module>> {
+    throw new Error("TODO")
+  }
+
+  async commit(): Promise<void> {
+    throw new Error("TODO")
+  }
+}
