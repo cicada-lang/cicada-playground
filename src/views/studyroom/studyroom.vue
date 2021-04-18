77ae2223257ce7187af5ef18fd56f1e584161979
Xie Yuheng <xyheme@gmail.com>
2021-04-18 18:30:55 +0800

    up

--- src/views/studyroom/studyroom.vue
+++ src/views/studyroom/studyroom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:px-8 md:py-6 flex flex-col items-start px-4 py-3 w-screen">
+  <div class="md:px-8 md:py-6 flex flex-col items-start w-screen px-4 py-3">
     <page-header>
       <button class="hover:text-gray-500">
         <router-link :to="{ path: '/entrance' }">BACK</router-link>
