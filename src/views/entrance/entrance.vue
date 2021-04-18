489d7bc7fb3be3037d55d1b2004bea5df0546a7f
Xie Yuheng <xyheme@gmail.com>
2021-04-19 00:24:09 +0800

    up

--- src/views/entrance/entrance.vue
+++ src/views/entrance/entrance.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:p-6 flex flex-col items-start p-2">
+  <div class="md:px-6 md:py-4 flex flex-col items-start p-2">
     <page-header />
 
     <h1 class="py-2 text-xl font-bold">//// Entrance</h1>
