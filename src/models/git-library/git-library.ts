d2cc25d17fd845c4493219fc7543e2dfcdacfd30
Xie Yuheng <xyheme@gmail.com>
2021-04-21 18:37:15 +0800

    up

--- src/models/git-library/git-library.ts
+++ src/models/git-library/git-library.ts
@@ -1,7 +1,9 @@
 import { Library } from "@cicada-lang/cicada"
+import { Checkout } from "./checkout"
 import { Stage } from "./stage"
 
 export interface GitLibrary extends Library {
   stage: Stage
+  checkout: Checkout
   commit(): Promise<void>
 }
